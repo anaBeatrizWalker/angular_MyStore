b3420db893c16e47a822bc3afc28c1f6f0d3acf6
anaBeatrizWalker <95242519+anaBeatrizWalker@users.noreply.github.com>
2022-10-09 12:04:46 -0300

    Adicionando formulário do checkout

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -10,7 +11,23 @@ export class CartComponent implements OnInit {
   //Armazena os produtos no carrinho
   items = this.cartService.getItems();
 
-  constructor(private cartService: CartService) {}
+  //Propriedade recebe um modelo de formulário com dois campos (name e address)
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {}
+
+  onSubmit(): void {
+    //Redefine o formulário e limpa o carrinho
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
