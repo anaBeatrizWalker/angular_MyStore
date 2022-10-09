821f9595b648916db5c4c443febd95c365598a8b
anaBeatrizWalker <95242519+anaBeatrizWalker@users.noreply.github.com>
2022-10-09 11:08:19 -0300

    Adicionando exibição do Componente da rota

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-  }
+    //Primeiro pega o id do produto atual da rota
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
 
+    //Encontra o produto que corresponde ao id passado na rota
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
 }
